736ef5746f780483837014723af739021aa421d6
shivankgautam12345 <142514350+shivankgautam12345@users.noreply.github.com>
2023-10-18 11:31:22 +0530

    Add files via upload

---
+++ ConvertRomanToIntApp.java
@@ -0,0 +1,18 @@
+package com.cloudvandana.solutions;
+
+
+
+import java.util.Scanner;
+
+public class ConvertRomanToIntApp {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String str=sc.next();
+		System.out.println(ConvertRomanToInt.romanToInteger(str));
+
+	}
+
+
+}
+
